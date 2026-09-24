178dcfafcfc29e41c5c5e71e203a005e7f51b0f7
Lakshya Jaiswal <jaiswallakshya221206@gmail.com>
2026-09-24 14:30:29 +0530

    Time: 4 ms (83.73%) | Memory: 44.5 MB (65.83%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int [] arr = new int [26];
+        int [] arr1 = new int [26];
+        for(int i=0; i<s.length(); i++){
+            arr[s.charAt(i)-'a']++;
+        }
+        for(int i=0; i<t.length(); i++){
+            arr1[t.charAt(i)-'a']++;
+        }
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]!=arr1[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
